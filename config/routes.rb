bb8f9478bf3c0d39eb5b51cdd85ade8a13d8ed3b
Pierre Libran <pedro89@hotmail.fr>
2026-09-14 16:08:12 +0200

    improving SEO

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get '/buy', to: redirect('https://gumroad.com/l/topotree')
   get '/download', to: redirect('https://github.com/RyoSaeba35/TopoTree_releases/releases/download/v0.1.0/Topotree_0.1.0_x64-setup.exe')
 
+  # /en redirects
+  get '/en',       to: redirect('/')
+  get '/en/*path', to: redirect('/%{path}')
+
   # Default locale (en) at root
   root "pages#home"
   get "/privacy",          to: "pages#privacy",         as: :privacy
